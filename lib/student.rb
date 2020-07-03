e55bda411528018e9bebe685fc43786b8856c90a
Hyung Kim <hjkmines@gmail.com>
2020-07-03 03:34:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ end
 
 def self.create_table 
   sql = <<-SQL 
-  CREATE TABLE IF NOT EXIST students (
+  CREATE TABLE IF NOT EXISTS students (
   ID INTEGER PRIMARY KEY, 
   name TEXT, 
   grade INTEGER
